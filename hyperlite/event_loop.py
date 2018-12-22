6f3c9c3894b10404250e390ea1c4bdfa12c5a69b
Anikesh Patel <anikeshpatel4@gmail.com>
2018-12-22 16:45:18 +0530

    add process module

--- hyperlite/event_loop.py
+++ hyperlite/event_loop.py
@@ -1,10 +1,12 @@
 """ Event Loop The Heart Of HyperLite DB """
 
+from .process import Process
+
 class EventLoop:
     def __init__(self):
         self.events = []
         self.callbacks = []
-        self.system_process = []
+        self.system_process: Process = []
 
     def execute_event(self):
         self.events.clear()
@@ -24,11 +26,10 @@ class LoopRunner:
         self.loop = EventLoop()
 
     def run(self):
-        print(self.shouldContinue())
         while(self.shouldContinue()):
             self.loop.execute_event()
             self.loop.execute_callbacks()
             self.loop.execute_system_process()
 
-    def shouldContinue(self):
+    def shouldContinue(self) -> bool:
         return (self.loop.events.__len__() != 0) or (self.loop.callbacks.__len__() != 0) or (self.loop.system_process.__len__() != 0)
---
+++ hyperlite/process.py
@@ -0,0 +1,15 @@
+""" Its all about processes and tasks  """
+
+from enum import Enum
+
+class ProcessType(Enum):
+    Query = 1
+    Writing = 2
+
+
+class Process:
+    def __init__(self):
+        self.type: ProcessType.Query
+
+    def exec(self):
+        pass
