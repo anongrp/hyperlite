dc81f80d91291893608426f2a8c83c4e5fe59f07
Gaurav Thakur <gauravthakur40127@gmail.com>
2019-01-02 09:27:56 +0530

    Collection class update(2)

--- hyperlite/collection.py
+++ hyperlite/collection.py
@@ -2,22 +2,27 @@
 
 class Collection:
     
-    def __init__(self, __tb_name: str):
-        self.__tb_name=__tb_name
-        self.columns = []
+    def __init__(self, col_name: str):
+        self.col_name = col_name
+        self.objects = []
 
     def __str__(self):
-        return self.__tb_name
+        return self.col_name
 
-    def setTBName(self,__tb_name: str):
-        self.__tb_name=__tb_name
+    def setTBName(self, col_name: str):
+        self.col_name = col_name
 
     def getTBName(self):
-        return self.__tb_name
+        return self.col_name
     
     def insert(self,meta_data: dict, user_data: dict):
         Database, Collection = self.__meta_separator(meta_data)
-        pass
+        for(collection in db.getCollections()):
+            if collection == Collection:
+                pass
+            else:
+                pass        
+    
     
     def __meta_separator(self, meta_data: dict) -> list:
         return [meta for meta in meta_data.values()]
