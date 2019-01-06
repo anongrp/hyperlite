36f8cbff0dd052390b13b34387ee469bf2ba824e
Gaurav Thakur <gauravthakur40127@gmail.com>
2019-01-06 09:28:16 +0530

    Process class update(2)

--- hyperlite/process.py
+++ hyperlite/process.py
@@ -1,11 +1,28 @@
 """ Its all about processes and tasks  """
 
-from enum import Enum
+from .request_parser import ParsedData
+from .collection import Collection
+from .collection import Collections
+from .database import Databases
+
 
 class Process:
-    def __init__(self, process_name: ProcessType, process_data: dict):
-        self.process_name: ProcessType = process_name
+    def __init__(self, process_data: ParsedData):
         self.process_data: dict = process_data
 
     def exec(self):
-        pass
+        if self.process_data.request_type == 'Read':
+            pass
+        elif self.process_data.request_type == 'Insert':
+            db_name, col_name = Collection.meta_separator(self.process_data.meta_data)
+            db = Databases.get_db(db_name)
+            col = Collections.get_collection(col_name)
+            if col.parent is db:
+                col.insert(self.process_data.user_data)
+                print('Yup, It works...')
+        elif self.process_data.request_type == 'Delete':
+            pass
+        elif self.process_data.request_type == 'Update':
+            pass
+        else:
+            pass
