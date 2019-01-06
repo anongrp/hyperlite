2176161d523e8ed180dc3036807248a82f7aeb2a
Gaurav Thakur <gauravthakur40127@gmail.com>
2019-01-06 09:27:17 +0530

    Database class update(4)

--- hyperlite/database.py
+++ hyperlite/database.py
@@ -2,33 +2,28 @@
 
 from time import gmtime, strftime
 
+
 class Database:
 
-    def __init__(self, __db_name: str, __collections: dict = {}):
-        self.__db_name = __db_name
-        self.__collections = __collections
-        self.__creation_date: str = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    def __init__(self, db_name: str):
+        self.db_name = db_name
+        self.creation_date: str = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        Databases.add_db(self)
 
     def __str__(self):
-        return self.__db_name+'\n'+str(self.__creation_date)
+        return self.db_name + '\n' + str(self.creation_date)
 
-    def getDBName(self):
-        return self.__db_name
-    
-    def setDBName(self, __db_name):
-        self.__db_name = __db_name
 
-    def getCreationDetails(self):
-        return self.__creation_date
+class Databases:
+    """   Maintains record of all open Databases   """
+    db_list = {}
 
-    def getCollections(self):
-        return list(self.__collections.keys())
-    
-    def getCollectionInfo(self, collection_name: str):
-        return dict['collection_name']
-    
-    def dropDatabase(self):
-        pass
-    
-    def addCollection(*args, **kwargs):
-        pass
+    @classmethod
+    def add_db(cls, db: Database):
+        Databases.db_list.update({
+            db.db_name: db
+        })
+
+    @classmethod
+    def get_db(cls, db_name: str):
+        return Databases.db_list.get(db_name)
