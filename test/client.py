693f33642aab35dd27195915439b9354d1088bed
Sohel Shaikh <34995462+Sohelll@users.noreply.github.com>
2019-02-19 03:27:17 +0530

    created client.py

---
+++ test/client.py
@@ -0,0 +1,21 @@
+import socket
+
+def Main():
+    host = '127.0.0.1'  #of server
+    port = 5000
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    message = input("-> ")
+
+    while message != 'q':
+        s.send(message.encode('utf-8'))
+        #data = s.recv(1024).decode('utf-8')
+        print("-"*30)
+        message = input("-> ")
+
+    s.close()
+
+if __name__ == '__main__':
+    Main()
