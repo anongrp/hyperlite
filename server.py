0ef9fd56b5b1fcf93e4b25f02079b6ba0d12c6bb
Anikesh Patel <anikeshpatel4@gmail.com>
2019-02-20 09:40:52 +0530

    polishing the code

--- server.py
+++ server.py
@@ -1,81 +1,39 @@
-import sys
-sys.path.append('..')
-from hyperlite.process import Process
-from hyperlite.request_parser import Parser
-from hyperlite.collection import Collection
-from hyperlite.database import Database
-import socket
+import socket, json
+from hyperlite.event import Event
 
 
-# def goto(linenum):  #a simple implementation of goto statement for easily writing code
-#     global line
-#     line = linenum
+class Socket(socket.socket):
+    def __init__(self, host, port):
+        super().__init__(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = port
+        self.host = host
+        self.clients = []
+        self._event = Event()
 
-
-def Main():
-    global line
-    line = 1
-
-    host = '127.0.0.1'
-    port = 5004
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host, port))
-
-    with open('test2.json', 'r') as f1:  # taking the user's data file to store
-        data = f1.read()
-
-    while True:
-        print("Server listening!!!!")
-
-        # listening to 1 client at a time
-        s.listen(1)
-        # accepting a clients request when he run connect() function
-        c, addr = s.accept()
-
-        print(type(addr))
-        print("Connection from: " + str(addr))
-
-        # queries = []
-        # for testing, populating the queries list
-        queries = [
-            """
-db = Database('db-1')  
-col = Collection('col-1', db)
-data = Parser.parse(data)
-process1= Process(data)
-print(process1.exec())
-""",
-        ]
-
-        # start an indefinite connection, (till explicitly terminated)
+    def listen(self, **kwargs):
+        super().bind((self.host, self.port))
+        print("Server is listening on port {}".format(self.port))
         while True:
-            client_command = c.recv(1024).decode('utf-8')
+            super().listen(1)
+            client, addr = super().accept()
+            self.clients.append({
+                "client": client,
+                "addr": addr
+            })
+            while True:
+                raw_query = client.recv(1024).decode("UTF-8")
+                if raw_query.lower() == 'exit':
+                    client.close()
+                    break
+                json_query = json.load(raw_query)
+                if json_query.type is not None and json_query.type.lower() == 'Request':
+                    self._event.emmit('request', json_query)
+                # code to communicate with hyperlite engine
 
-            if not data or 'end connection' in client_command:
-                break
+    def on_request(self, callback):
+        self._event.on("request", callback)
 
-            if 'execute' in client_command:
-                for code in queries:
-                    print(code)
-
-                    exec(code)  # this will execute the client commands
-                    # empty the list after executing the code
-                queries = []
-            else:
-                queries.append(client_command)
-
-            for lines in queries:
-                print("----> " + str(lines))
-
-            print("From connected user: " + client_command)
-                # c.send(data.encode('utf-8'))
-            
-
-    c.close()
 
 if __name__ == '__main__':
-    Main()
-
-
-
+    socket = Socket(host="localhost", port=4444)
+    socket.listen()
--- test/client.py
+++ test/client.py
@@ -2,19 +2,21 @@ import socket
 
 def Main():
     host = '127.0.0.1'  #of server
-    port = 5004
+    port = 4444
 
     s = socket.socket()
     s.connect((host, port))
 
     message = input("-> ")
 
-    while message != 'q':
+    while True:
         s.send(message.encode('utf-8'))
-        #data = s.recv(1024).decode('utf-8')
+        data = s.recv(1024).decode('utf-8')
+        print(data)
         print("-"*30)
+        if message.lower() == 'exit':
+            break
         message = input("-> ")
-
     s.close()
 
 if __name__ == '__main__':
