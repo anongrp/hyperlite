18d45f2d03e913396e424179a7c5770e682337eb
Anikesh Patel <anikeshpatel4@gmail.com>
2019-02-21 18:26:03 +0530

    adding new process module

--- main.py
+++ main.py
@@ -11,16 +11,34 @@ from hyperlite.request_parser import Parser
 from hyperlite import config
 from hyperlite.event import Event
 from server import Socket
+from hyperlite.collection import Collection
+from hyperlite.process import write_process
 
 if __name__ == "__main__":
     socket = Socket(config.DEFAULT.get('host'), config.DEFAULT.get('port'))
     loop_runner = event_loop.LoopRunner()
     loop_runner.run()
 
+
+    def manage_loop_status():
+        if not loop_runner.isRunning:
+            Event.emmit('loop-rerun')
+
     def onRequest(data):
         loop_runner.loop.query_processes.append(process.Process(Parser.parse(data)))
         if not loop_runner.isRunning:
             Event.emmit('loop-rerun')
 
+    def onCollectionChange(collection: Collection):
+        loop_runner.loop.system_process.append(write_process.rendererProcess(collection))
+
+
     Event.on('request', onRequest)
+    Event.on('col-change', onCollectionChange)
+
     socket.listen()
+
+    # Code of releasing the F.A.L.T.U listeners
+    Event.off('request')
+    Event.off('col-change')
+
